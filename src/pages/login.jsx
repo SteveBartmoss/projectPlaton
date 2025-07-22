245e4266de5ffad88d787cb96660baf600190550
Marco Antonio Guadalupe Rivas Guzman <marco.rivas@carsol.mx>
2025-07-22 14:39:54 -0600

    feat: Se implemento la vista para el login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,16 +1,18 @@
-
+import './login.css'
 
 export function Login(){
     return(
-        <div>
+        <div className='card-div'>
             <div>
                 <h1>Ingresar</h1>
             </div>
             <div>
                 <input type="text" />
-
+            </div>
+            <div>
                 <input type="password" />
-
+            </div>
+            <div>
                 <button>login</button>
             </div>
         </div>
