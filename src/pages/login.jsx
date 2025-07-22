6113c27d9c7521979ae9c12ba5c296b238e88a07
Marco Antonio Guadalupe Rivas Guzman <marco.rivas@carsol.mx>
2025-07-22 14:29:25 -0600

    feat: Se inicializo el proyecto

---
+++ src/pages/login.jsx
@@ -0,0 +1,18 @@
+
+
+export function Login(){
+    return(
+        <div>
+            <div>
+                <h1>Ingresar</h1>
+            </div>
+            <div>
+                <input type="text" />
+
+                <input type="password" />
+
+                <button>login</button>
+            </div>
+        </div>
+    )
+}
