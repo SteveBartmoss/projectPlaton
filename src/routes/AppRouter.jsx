73ec1aef0b6b7d8ef9635958a03a4e608cb8b40a
Marco Antonio Guadalupe Rivas Guzman <marco.rivas@carsol.mx>
2025-07-23 08:03:50 -0600

    feat: Se implemento una ruta por defecto, en caso de que no se encuentre nada, muestra la pagina de not found

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,11 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "../pages/home";
+import { NotFound } from "../pages/NotFound";
 
 
 export function AppRouter(){
     return(
         <Routes>
             <Route path="home" element={<Home />} />
+
+            <Route path="*" element={<NotFound />} />
         </Routes>
     )
 }
