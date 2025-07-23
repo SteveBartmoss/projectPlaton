0a1720f108c183b53223e24016b356dce8fbd4e5
Marco Antonio Guadalupe Rivas Guzman <marco.rivas@carsol.mx>
2025-07-23 08:35:05 -0600

    feat: Se implemento el uso del contenedor principal de la aplicacion

--- src/routes/MainRouter.jsx
+++ src/routes/MainRouter.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Login } from "../pages/login";
 import { Home } from "../pages/home";
 import { AppRouter } from "./AppRouter";
+import { DivApp } from "../components/containers/container";
 
 export function MainRouter(){
     return(
         <>
+        <DivApp>
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Login />} />
@@ -13,6 +15,7 @@ export function MainRouter(){
                     <Route path="/*" element={<AppRouter />}/>
                 </Routes>
             </BrowserRouter>
+        </DivApp>
         </>
     )
 }
